560ed5d5869a46d2014357a05446d2d90782e01a
George T. Gougoudis <george_gougoudis@hotmail.com>
2019-05-14 19:32:34 +0300

    Initial commit

---
+++ cfs/src/cfs.c
@@ -0,0 +1,9 @@
+#include <stdlib.h>
+#include <openssl/sha.h>
+
+#include "cfs.h"
+
+
+int init_cfs(char *rootDir) {
+
+}
---
+++ cfs/src/cfs.h
@@ -0,0 +1,12 @@
+#ifndef __CFS_H__
+#define __CFS_H__
+
+#define CFS_ROOT ".cfs"
+#define CFS_DIRECTORY "cfs_dir"
+
+
+struct cfs_state {
+
+}
+
+#endif
